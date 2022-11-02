4975176461376a230acbb799599eea173cde6621
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-11-02 18:10:09 +0100

    remove \r, and explore kinds of nodes

--- src/parser.js
+++ src/parser.js
@@ -3,10 +3,21 @@
 async function sif_parser(ev){
     let file = ev.target.files[0];
     let txt = await file.text();
-
-    //console.log(txt);
+    txt = txt.split('\r').join('');
 
     var interactions = SIFJS.parse(txt);
     console.log(interactions)
+
+    /*
+    Pour arcourir nodes et voir ce qu'on veut
+    for(var i = 0; i < interactions.nodes.length; i++){
+        var nodeType = interactions.nodes[i].id.split("(")[0];
+        if(["bp"].includes(nodeType) ){
+            console.log(interactions.nodes[i].id);
+        }
+        
+    }
+    */
+    
     return txt;
 }
