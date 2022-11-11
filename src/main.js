a41a8cc4988ee3f7052eab36508eaf02d4492f33
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-11-11 11:10:02 +0100

    Récupération des résultats de sif_parser

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ function initialize(){
     form.setAttribute("type","file");
     form.setAttribute("class","browse");
     form.setAttribute("id","upload_input");
-    form.addEventListener("change", sif_parser);
+    form.addEventListener("change", on_file_upload);
 
     let upload_zone = document.getElementById("upload_zone");
     upload_zone.appendChild(form);
 }
+
+async function on_file_upload(ev){
+    // Codé en dur pour l'instant, à remplacer par un choix interactif de l'utilisateur et/ou une config file
+    let filter_nodes = ['bp(Reactome:"Defective AHCY causes HMAHCHD.")','bp(Reactome:"Sulfur amino acid metabolism.")','bp(GOBP:"one-carbon metabolic process")'];
+
+    let file = ev.target.files[0];
+    let interactions = await sif_parser(file, filter_nodes);
+    
+    console.log(interactions);
+}
+
 ///////////////////////////////////////////////////     MAIN     ///////////////////////////////////////////////////
 initialize()
--- src/parser.js
+++ src/parser.js
@@ -4,7 +4,64 @@ function safe_push(array, element){
         array.push(element)
     }
 }
+async function sif_parser(file, filter_nodes){
+    // Parse
+    raw_text = await file.text();
+    let interactions = SIFJS.parse(raw_text);
+    
 
+    // Remove pathologies
+    for(let i = 0; i < interactions.links.length; i++){
+        if(interactions.links[i].source.includes("path(") || interactions.links[i].target.includes("path(")){
+            interactions.links.splice(i,1);
+            i--;
+        }
+    }
+
+    interactions.nodes = [];
+    for(let i = 0; i < filter_nodes.length; i++){
+        safe_push(interactions.nodes, {id : filter_nodes[i]});
+    }
+
+    //Ajouter à interactions.nodes les nodes en interaction avec bp
+    for(let i = 0; i < interactions.links.length; i++){
+        if(filter_nodes.includes(interactions.links[i].source)){
+            safe_push(interactions.nodes, {id : interactions.links[i].target})
+        }
+        if(filter_nodes.includes(interactions.links[i].target)){
+            safe_push(interactions.nodes, {id : interactions.links[i].source})
+
+        }
+    }
+
+    // Remove edges n'utilisant pas les nodes choisis
+    for(let i = 0; i < interactions.links.length; i++){
+        if(interactions.nodes.find(e => e.id === interactions.links[i].source)){
+            continue;
+        }
+        if(interactions.nodes.find(e => e.id === interactions.links[i].target)){
+            continue;
+        }
+        interactions.links.splice(i,1);
+        i--;
+    }
+
+    // Add node en interactions
+    for(let i = 0; i < interactions.links.length; i++){
+        if(interactions.nodes.find(e => e.id === interactions.links[i].source)){
+            safe_push(interactions.nodes, {id : interactions.links[i].target});
+        }
+        if(interactions.nodes.find(e => e.id === interactions.links[i].target)){
+            safe_push(interactions.nodes, {id : interactions.links[i].source});
+        }
+    }
+    return interactions;
+
+
+}   
+
+
+/*
 async function sif_parser(ev){
     // Parse
     let file = ev.target.files[0];
@@ -66,3 +123,4 @@ async function sif_parser(ev){
     console.log(interactions)
     return interactions;
 }
+*/
