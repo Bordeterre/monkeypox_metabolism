e9322c3c34ce909d4a6e3054f052b812c3558bbf
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-11-11 11:59:25 +0100

    fetch_CHembl() + enlève les 'r' du sif_parser()

--- src/main.js
+++ src/main.js
@@ -8,17 +8,24 @@ function initialize(){
 
     let upload_zone = document.getElementById("upload_zone");
     upload_zone.appendChild(form);
+
+    
 }
 
 async function on_file_upload(ev){
+    ///// TESTE LE FETCH /////
+    let result = await fetch_CHembl("CHEMBL1201130");
+    console.log(result);
+
+    ///// TESTE LE PARSEUR /////
     // Codé en dur pour l'instant, à remplacer par un choix interactif de l'utilisateur et/ou une config file
     let filter_nodes = ['bp(Reactome:"Defective AHCY causes HMAHCHD.")','bp(Reactome:"Sulfur amino acid metabolism.")','bp(GOBP:"one-carbon metabolic process")'];
-
+    
     let file = ev.target.files[0];
     let interactions = await sif_parser(file, filter_nodes);
-    
     console.log(interactions);
 }
 
 ///////////////////////////////////////////////////     MAIN     ///////////////////////////////////////////////////
 initialize()
+
--- src/parser.js
+++ src/parser.js
@@ -4,9 +4,11 @@ function safe_push(array, element){
         array.push(element)
     }
 }
+
 async function sif_parser(file, filter_nodes){
     // Parse
     raw_text = await file.text();
+    raw_text = raw_text.split("\r").join("");
     let interactions = SIFJS.parse(raw_text);
     
 
@@ -60,67 +62,9 @@ async function sif_parser(file, filter_nodes){
 
 }   
 
+async function fetch_CHembl(id){
+    let result = await fetch("https://www.ebi.ac.uk/chembl/api/data/molecule/" + id + ".json")
+        .then((response) => response.json());
 
-/*
-async function sif_parser(ev){
-    // Parse
-    let file = ev.target.files[0];
-    let txt = await file.text();
-    var interactions = SIFJS.parse(txt);
-    console.log(interactions)
-
-    // Supprime les associations au pathologies
-    for(let i = 0; i < interactions.links.length; i++){
-        if(interactions.links[i].source.includes("path(")){
-            interactions.links.splice(i,1);
-            i--;
-        }
-        if (interactions.links[i].target.includes("path(")){
-            interactions.links.splice(i,1);
-            i--;
-        }
-    }
-
-    let filter_nodes = ['bp(Reactome:"Defective AHCY causes HMAHCHD.")','bp(Reactome:"Sulfur amino acid metabolism.")','bp(GOBP:"one-carbon metabolic process")'];
-    interactions.nodes = [];
-    for(let i = 0; i < filter_nodes.length; i++){
-        safe_push(interactions.nodes, {id : filter_nodes[i]});
-    }
-
-    //Ajouter à interactions.nodes les nodes en interaction avec bp
-    for(let i = 0; i < interactions.links.length; i++){
-        if(filter_nodes.includes(interactions.links[i].source)){
-            safe_push(interactions.nodes, {id : interactions.links[i].target})
-        }
-        if(filter_nodes.includes(interactions.links[i].target)){
-            safe_push(interactions.nodes, {id : interactions.links[i].source})
-
-        }
-    }
-
-    // Remove edges n'utilisant pas les nodes choisis
-    for(let i = 0; i < interactions.links.length; i++){
-        if(interactions.nodes.find(e => e.id === interactions.links[i].source)){
-            continue;
-        }
-        if(interactions.nodes.find(e => e.id === interactions.links[i].target)){
-            continue;
-        }
-        interactions.links.splice(i,1);
-        i--;
-    }
-
-    // Add node en interactions
-    for(let i = 0; i < interactions.links.length; i++){
-        if(interactions.nodes.find(e => e.id === interactions.links[i].source)){
-            safe_push(interactions.nodes, {id : interactions.links[i].target});
-        }
-        if(interactions.nodes.find(e => e.id === interactions.links[i].target)){
-            safe_push(interactions.nodes, {id : interactions.links[i].source});
-        }
-    }
-
-    console.log(interactions)
-    return interactions;
+    return result;
 }
-*/
