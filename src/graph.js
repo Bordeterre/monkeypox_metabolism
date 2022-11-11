d5974c0f0bac09b16c223a14ad4ac84bd462117e
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-11-11 23:04:48 +0100

    fetch protein names and UniProt ids from CHembl

--- src/graph.js
+++ src/graph.js
@@ -7,15 +7,17 @@ class Graph {
         return this.elements.find(e => e.id === id);  
     }
 
-    create_node(id){
+    async create_node(id){
         let node = this.search_element(id)
 
         if(typeof node === "undefined"){
             if(id.startsWith("a(")){
-                node = new Metabolite(id); 
+                node = new Metabolite(id);
+                await node.fetch_from_Chembl(); 
             }
             else if(id.startsWith("p(")){
                 node = new Protein(id);
+                await node.fetch_from_Chembl();
             }
             else if(id.startsWith("bp(")){
                 node = new Pathway(id);
@@ -53,8 +55,8 @@ class Graph {
             let target = lines[l][2];
 
             if(pathways_of_interest.includes(source) || pathways_of_interest.includes(target)){
-                let source_node = this.create_node(source)
-                let target_node = this.create_node(target)
+                let source_node = await this.create_node(source);
+                let target_node = await this.create_node(target);
                 this.create_edge(source_node, target_node);
             }
         }
@@ -72,7 +74,7 @@ class Graph_element {
 
 class Edge extends Graph_element {
     constructor(source, target) {
-        super(source + "=>" + target,"association"),
+        super(source.id + "=>" + target.id,"association"),
         this.source = source,
         this.target = target,
         this.cardinality = 0
@@ -96,17 +98,36 @@ class Displayable_node extends Node {
     constructor(id, class_name, label, clonemarker){
         super(id, class_name, label, clonemarker)
     }
+
+    
 }
 
 class Protein extends Displayable_node {
     constructor(id){
-        super(id, "macromolecule", "fetch_CHembl(id)", false)
+        super(id, "macromolecule", "label", false);
+    }
+
+    async fetch_from_Chembl(){
+        let id_CHembl = this.id.split(":")[1].slice(0, -1);
+        let json = await fetch("https://www.ebi.ac.uk/chembl/api/data/target/search.json?organism=Homo%20sapiens&q=" + id_CHembl)
+        .then((response) => response.json());
+        //console.log(json.targets[0])
+        this.label = json.targets[0].pref_name;
+        if(json.targets[0].cross_references.length > 0){
+            this.uniprot_id = json.targets[0].cross_references[0].xref_id;
+        } else {
+            this.uniprot_id = null;
+        }
     }
 }
 
 class Metabolite extends Displayable_node {
     constructor(id){
         super(id, "simple chemical", "fetch_CHembl(id)", "fetch_CHembl(id)")
+    }
+
+    async fetch_from_Chembl(){
+
     }
 }
 
