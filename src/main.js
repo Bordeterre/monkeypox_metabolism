49bc3d27e18cee89d47b245eb2edf59547678d3a
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-11-15 23:46:32 +0100

    fix : asynchronous graph display

--- src/main.js
+++ src/main.js
@@ -31,8 +31,147 @@ async function on_file_upload(ev){
 
 async function display_graph(){
     let json = write_json(GRAPH).elements;
-    console.log(json);
+    
 
+    let json = [
+        // nodes
+        /*"data": {
+            "id": "glyph23",                   // id of the node
+            "class": "simple chemical",        // class of the node (see classes section for a list of supported sbgn glyphs
+            "label": "Ca2+",                   // label to be displayed on the node
+            "parent": "glyph2",                // parent node id if any
+            "clonemarker": false,              // whether the node has a clonemarker or not
+            "stateVariables": [],              // an array of state variables
+            "unitsOfInformation": [],          // an array of units of information
+          }*/
+        { data: { 
+            id: 'a(ChEMBLAssay:CHEMBL3294635)',
+            class: 'simple chemical',
+            label: 'CHEMBL3294635',
+            clonemarker:"false",
+            stateVariables: [],
+            unitsOfInformation: [],
+        } 
+    },
+        //{ data: { id: 'a(ChEMBLAssay:CHEMBL3294636)' } },
+        { data: { 
+            id: 'p(HGNC:AHCY)',
+            class: 'macromolecule',
+            label: 'AHCY',
+            clonemarker:"false",
+            stateVariables: [],
+            unitsOfInformation: [],
+        } 
+    },
+      { data: { 
+        id: 'a(ChEMBLAssay:CHEMBL3294636)',
+        class: 'simple chemical',
+        label: 'CHEMBL3294636',
+        clonemarker:"false",
+        stateVariables: [],
+        unitsOfInformation: [],
+      } 
+    },
+      { data: { 
+        id: 'a(ChEMBLAssay:CHEMBL800629)',
+        class: 'simple chemical',
+        label: 'CHEMBL800629',
+        clonemarker:"false",
+        stateVariables: [],
+        unitsOfInformation: [],
+      } 
+    },
+      { data: { 
+        id: 'bp(Reactome:Methylation.)',
+        class: 'process',
+        label: 'Reactome:Methylation',
+        clonemarker:"false",
+        stateVariables: [],
+        unitsOfInformation: [],
+      } 
+    },
+        //{ data: { id: 'a(ChEMBLAssay:CHEMBL800629)' } },
+        //{ data: { id: 'bp(Reactome:Methylation.)' } },
+        //{ data: { id: 'bp(Reactome:\"Sulfur amino acid metabolism.\")' } },
+        // edges
+        /*"data": {
+            "id": "glyph19-glyph5",            // id
+            "class": "production",             // sbgn class
+            "cardinality": 0,                  // cardinality
+            "source": "glyph19",               // source node id
+            "target": "glyph5",                // target node id
+            "portSource": "glyph19",           // port of the source
+            "portTarget": "glyph5"             // port of the target
+          }*/
+        /*1: Object { target: "a(ChEMBLAssay:CHEMBL3294636)", source: "p(HGNC:AHCY)", intType: "association" }
+    2: Object { target: "a(ChEMBLAssay:CHEMBL800629)", source: "p(HGNC:AHCY)", intType: "association" }
+    3: Object { target: "bp(Reactome:Methylation.)", source: "p(HGNC:AHCY)", intType: "association" }*/ 
+        {
+          data: {
+            id: 'association',
+            class: "association",
+            cardinality: 0, 
+            source: 'p(HGNC:AHCY)',
+            target: 'a(ChEMBLAssay:CHEMBL3294635)'
+          }
+        },        
+        {
+          data: {
+            id: 'association',
+            class: "association",
+            cardinality: 10, 
+            source: 'p(HGNC:AHCY)',
+            target: 'a(ChEMBLAssay:CHEMBL3294636)'
+          }
+        },
+        {
+          data: {
+            id: 'association',
+            class: "association",
+            cardinality: 20, 
+            source: 'p(HGNC:AHCY)',
+            target: 'a(ChEMBLAssay:CHEMBL800629)'
+          }
+        },
+        {
+          data: {
+            id: 'association',
+            class: "association",
+            cardinality: 30, 
+            source: 'p(HGNC:AHCY)',
+            target: 'bp(Reactome:Methylation.)'
+          }
+        },
+        /*
+        {
+          data: {
+            id: 'association_1',
+            source: 'a(ChEMBLAssay:CHEMBL3294636)',
+            target: 'p(HGNC:AHCY)'
+          }
+        },
+        {
+          data: {
+            id: 'association_2',
+            source: 'a(ChEMBLAssay:CHEMBL800629)',
+            target: 'p(HGNC:AHCY)'
+          }
+        },
+        {
+          data: {
+            id: 'association_3',
+            source: 'bp(Reactome:Methylation.)',
+            target: 'p(HGNC:AHCY)'
+          }
+        },
+        {
+          data: {
+            id: 'association_4',
+            source: 'bp(Reactome:\"Sulfur amino acid metabolism.\")',
+            target: 'p(HGNC:AHCY)'
+          }
+        }*/
+      ]
     let cy = cytoscape({
         container : document.getElementById("graph_display"),
         elements : json,
