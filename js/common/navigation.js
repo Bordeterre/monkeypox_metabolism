68f934c2cdac54c21c02401549a1cac454707010
Nicolas Maurice <nicolas.mrcvd@gmail.com>
2022-12-01 22:52:04 +0100

    clarified go_to_loadingGraph()

--- js/common/navigation.js
+++ js/common/navigation.js
@@ -6,10 +6,12 @@ function go_to_pathwaySelection(graph, pathways){
     window.location.href = "html/pathwaySelection.html";
 }
 
-function go_to_loadinggraph(graph){
+function go_to_loadingGraph(graph, pathways){
+    //graph : la structure Graph
+    //pathways : les pathways sélectionnés
     window.localStorage.clear();
     window.sessionStorage.setItem("graph",JSON.stringify(graph));
-
+    window.sessionStorage.setItem("pathways",JSON.stringify(pathways));
     window.location.href = "html/loadingGraph.html";
 }
 
