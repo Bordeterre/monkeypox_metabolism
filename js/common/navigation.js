1b3c08ed8e292274dba88e705336a0fa72a7a29a
anallain <99040327+Noiselis@users.noreply.github.com>
2022-12-09 18:45:07 +0100

    new fonction to get name in json

--- js/common/navigation.js
+++ js/common/navigation.js
@@ -20,3 +20,11 @@ function go_to_displayGraph(json){
     window.sessionStorage.setItem("json",JSON.stringify(json));
     window.location.href = "../html/graph.html";
 }
+
+function get_name(id, json){
+    for (let i of json){
+        if (i.data.id == id){
+            return i.data.name;
+        }
+    }
+}
