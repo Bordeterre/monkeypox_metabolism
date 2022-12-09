d87031778f8f73d975201b912371b998d87390a4
anallain <99040327+Noiselis@users.noreply.github.com>
2022-12-09 18:44:21 +0100

    ajout fonction affichage nom entier

--- js/displayGraph/displayGraph.js
+++ js/displayGraph/displayGraph.js
@@ -7,6 +7,21 @@ function display_graph(json){
     });
     cy.layout({name: "cose"}).run();
 
+///////////////////////////////////////////////////// To display entire name of assays when node is clicked //////////////////////////////////////////////////////////////
+
+    cy.on('tap', 'node', function(evt){
+        document.getElementById("name_display").innerHTML = "";
+        var node_promised = evt.target;
+        let node_name = get_name(node_promised.id(), json)
+        let download_zone = document.getElementById("name_display");
+        let entire_name_assay = document.createTextNode(node_name);
+        download_zone.appendChild(entire_name_assay);
+    });
+
+}
+    
+    
+    
 }
 
 
